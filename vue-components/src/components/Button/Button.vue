2822928fb92e049da66ccc80ee05dc55751cd4b9
cl4pper <cl4pper@gmail.com>
2018-06-29 18:05:42 -0300

    Vue-components: ADD: Button component - start: only style.
    GENERAL: add sass, bootstrap packages.

---
+++ vue-components/src/components/Button/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="button-body">
+        <button :class="setBtn" @click="onClick"> {{ label }} </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'button',
+    props: {
+        label: {
+            type: String,
+            required: false,
+            default: "Press"
+        },
+        onClick: {
+            type: Function,
+            required: false,
+        },
+        color: {
+            type: String,
+            required: false,
+            default: "light",
+        }
+    },
+    computed: {
+        setBtn: function() {
+            return "btn button-" + this.color;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import './style.scss';
+</style>
