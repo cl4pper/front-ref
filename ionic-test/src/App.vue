a1605b5535dfd59eeddafe51c44d7ce313232fdd
Matheus Souza <matheus.souza@cronapp.io>
2018-07-13 15:27:50 -0300

    Studio-four project start - sass, bootstrap.

--- ionic-test/src/App.vue
+++ ionic-test/src/App.vue
@@ -17,6 +17,7 @@
             <ion-item>
               <ion-label full style="font-weight: bold;">ID</ion-label>
               <ion-label full style="font-weight: bold;">NAME</ion-label>
+              <ion-label full style="font-weight: bold;"></ion-label>
               <!-- <ion-label full>COL 3</ion-label> -->
             </ion-item>
             <!-- /Table Header -->
@@ -25,6 +26,8 @@
             <ion-item v-for="item of items" v-bind:key="item.id">
               <ion-label full>{{ item.id }}</ion-label>
               <ion-label full>{{ item.name }}</ion-label>
+              <ion-button small>Edit</ion-button>
+              <ion-button color="danger" small><ion-icon name="trash"></ion-icon></ion-button>
               <!-- <ion-label full>World</ion-label> -->
             </ion-item>
             <!-- /Table content -->
