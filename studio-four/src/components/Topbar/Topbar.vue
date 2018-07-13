f604a93b2033c4091b295f6ce195785108069d1b
Matheus Souza <matheus.souza@cronapp.io>
2018-07-13 16:51:56 -0300

    Topbar component start.

---
+++ studio-four/src/components/Topbar/Topbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="topbar">
+    <div class="topbar-header">
+      <scan class="topbar-header-text">{{ label }}</scan>
+    </div>
+    <div class="topbar-icon">
+      <scan class="topbar-icon-menu"><font-awesome-icon icon="bars" /></scan>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: "Header",
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import './style.scss';
+</style>
--- studio-four/src/main.js
+++ studio-four/src/main.js
@@ -1,5 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+// BOOTSTRAP USE
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// FONTAWESOME
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faBars);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
