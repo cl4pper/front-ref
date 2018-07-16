3b93d353118cdc973de92b7a690a9961beff31e7
cl4pper <cl4pper@gmail.com>
2018-07-16 00:16:30 -0300

    Artist: a. add sass loader; b. start Topbar component.

---
+++ artist-vue/src/components/Topbar/Topbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="topbar">
+        <h1>{{ label }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'topbar',
+    props: {
+        label: {
+            type: String,
+            required: true,
+            default: "Header",
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+@import './style.scss';
+</style>
