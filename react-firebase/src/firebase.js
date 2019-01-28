4ac5d0a94441a91ebe5245684e0b7038a4d96844
cl4pper <cl4pper@gmail.com>
2019-01-28 00:29:41 -0300

    [react-firebase] - Add sass relative packages
    change App component,
    change firebase config file

--- react-firebase/src/firebase.js
+++ react-firebase/src/firebase.js
@@ -1,13 +1,17 @@
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
+import Firebase from 'firebase'
 
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDv_YzMk3Xf__CC4TdUU1hhrVbJLSPskXo",
-    authDomain: "react-firebase-17d15.firebaseapp.com",
-    databaseURL: "https://react-firebase-17d15.firebaseio.com",
-    projectId: "react-firebase-17d15",
-    storageBucket: "react-firebase-17d15.appspot.com",
-    messagingSenderId: "453015460755"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+const FIREBASE_CONFIG = Firebase.initializeApp ({
+	apiKey: "AIzaSyDv_YzMk3Xf__CC4TdUU1hhrVbJLSPskXo",
+	authDomain: "react-firebase-17d15.firebaseapp.com",
+	databaseURL: "https://react-firebase-17d15.firebaseio.com",
+	projectId: "react-firebase-17d15",
+	storageBucket: "react-firebase-17d15.appspot.com",
+	messagingSenderId: "453015460755"
+})
+// firebase.initializeApp(config);
+
+export const FIREBASE_DB = FIREBASE_CONFIG.database()
+export const databaseRef = FIREBASE_DB.ref('names')
