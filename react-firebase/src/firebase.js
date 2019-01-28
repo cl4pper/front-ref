00234e694a2f25d4c10fe731131923ce56285845
cl4pper <cl4pper@gmail.com>
2019-01-28 00:02:26 -0300

    [react-firebase] - Initialize connection with firebase console
    create file to store firebase config temporally

---
+++ react-firebase/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase'
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDv_YzMk3Xf__CC4TdUU1hhrVbJLSPskXo",
+    authDomain: "react-firebase-17d15.firebaseapp.com",
+    databaseURL: "https://react-firebase-17d15.firebaseio.com",
+    projectId: "react-firebase-17d15",
+    storageBucket: "react-firebase-17d15.appspot.com",
+    messagingSenderId: "453015460755"
+  };
+  firebase.initializeApp(config);
