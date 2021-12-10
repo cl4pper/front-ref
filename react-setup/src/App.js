c0f1997a3377683c31e4c87c63148fbaf32a59b8
cl4pper <cl4pper@gmail.com>
2021-12-10 12:45:07 +0000

    feat[react-setup]: setup project components

---
+++ react-setup/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="App__title">react-setup</h1>
+        </div>
+    );
+};
+
+export default App;
