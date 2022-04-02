bed616d5dd83caf3e928f75c3a3ac11128e88356
cl4pper <cl4pper@gmail.com>
2022-04-02 21:31:40 +0100

    feat: move use-memo into react-hooks folder

--- react-hooks/src/store/modules/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { reducer as commentsReducer } from './comments';
-
-const rootReducer = combineReducers({
-	comments: commentsReducer,
-});
-
-export { rootReducer };
