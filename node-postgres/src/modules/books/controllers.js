5fe4060526b0392897579d525d38309879bb1d23
cl4pper <cl4pper@gmail.com>
2023-12-17 22:35:07 +0000

    feat[books]: create get, post and delete controllers
    * fix requests hanging issue
    * refact PostBook
    * delete book controller
    * get book by id controller
    * create clear all controller

--- node-postgres/src/modules/books/controllers.js
+++ node-postgres/src/modules/books/controllers.js
@@ -8,24 +8,65 @@ async function GetBooks(req, res) {
 
     return res.status(200).send(data.rows);
   } catch(err) {
-    return res.status(500).send(`Error at GetBook: ${err}`);
+    return res.status(500).send(`Error at GetBooks: ${err}`);
   }
 };
 
+async function GetBookById(req, res) {
+  const {id} = req.params;
+  try {
+    const data = await BooksServices.selectBookById(id);
+    if(!data.rowCount) return res.status(204).send({ message: 'Book does no exist'});
+
+    return res.status(200).send(data.rows[0]);
+  } catch(err) {
+    return res.status(500).send(`Error at GetBookById: ${err}`);
+  }
+};
 
 async function PostBook(req, res) {
-  const data = req.body;
-
+  const { title, author } = req.body;
   try {
-    if (!data.title || !data.author) return res.status(400);
-    await BooksServices.createBook(data);
-    return res.status(200).send();
+    if (!title || !author) return res.status(400).send({ message: 'Requires title and author'});
+
+    const data = await BooksServices.createBook({title, author});
+
+    return res.status(200).send(data);
   } catch(err) {
     return res.status(500).send(`Error at PostBook: ${err}`);
   }
 };
 
+async function DeleteBook(req, res) {
+  const {id} = req.params;
+  try {
+    if(!id) return res.status(400).send({ message: 'ID is required' });
+
+    const book = await BooksServices.selectBookById(id);
+    if (!book.rowCount) res.status(400).send({ message: 'Book does not exist' });
+
+    const data = await BooksServices.deleteBook(id);
+
+    return res.status(200).send(data);
+  } catch(err) {
+    return res.status(500).send(`Error at DeleteBook: ${err}`);
+  }
+};
+
+async function ClearBooks(_, res) {
+  try {
+    const data = await BooksServices.deleteAllBooks();
+
+    return res.status(200).send(data);
+  } catch(err) {
+    return res.status(500).send(`Error at ClearBooks: ${err}`);
+  }
+};
+
 module.exports = {
   GetBooks,
-  PostBook
+  GetBookById,
+  PostBook,
+  DeleteBook,
+  ClearBooks
 }
