6208e75c5a5090dd07f9fe18b984a9fea71fa291
cl4pper <cl4pper@gmail.com>
2023-12-17 22:31:16 +0000

    feat[books]: create select, create and delete queries
    * select book by id
    * delete book by id
    * create delete all

--- node-postgres/src/modules/books/services.js
+++ node-postgres/src/modules/books/services.js
@@ -4,12 +4,28 @@ async function selectBooks() {
   return await pool.query('SELECT * FROM books');;
 }
 
+async function selectBookById(id) {
+  return await pool.query(`SELECT * FROM books WHERE id=${id}`);
+}
+
 async function createBook(data) {
   const { title, author } = data;
   await pool.query('INSERT INTO books (title, author) VALUES ($1, $2)', [title, author]);
 }
 
+async function deleteBook(id) {
+  await pool.query(`DELETE FROM books WHERE id=${id}`);
+}
+
+// research required - how to delete all element from a table
+async function deleteAllBooks() {
+  await pool.query(``);
+}
+
 module.exports = {
   selectBooks,
-  createBook
+  selectBookById,
+  createBook,
+  deleteBook,
+  deleteAllBooks
 }
