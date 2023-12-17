916d51e411e24ff1c1d888e26e5f6363e863ff6c
cl4pper <cl4pper@gmail.com>
2023-12-17 22:38:14 +0000

    feat[book]: set get and delete routes
    * get book by id route
    * delete book by id route

--- node-postgres/src/modules/books/routes.js
+++ node-postgres/src/modules/books/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const BooksControllers = require('./controllers');
 
+router.get('/books/:id', BooksControllers.GetBookById);
+router.delete('/books/:id', BooksControllers.DeleteBook);
 router.get('/books', BooksControllers.GetBooks);
-router.post('/book', BooksControllers.PostBook);
+router.post('/books', BooksControllers.PostBook);
 
 module.exports = router;
