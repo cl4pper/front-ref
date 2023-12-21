0fce71f841d859f62d11d472b50b243ff8d1cb8c
cl4pper <cl4pper@gmail.com>
2023-12-21 10:48:22 +0000

    chore[node-postrgres]: setup Docker
    * set env variables
    * connect Node and Postgres containers

--- node-postgres/db.config.js
+++ node-postgres/db.config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  port: process.env.DATABASE_PORT,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_DOCKER_PORT,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
